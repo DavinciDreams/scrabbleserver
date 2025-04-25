09fc9553b1f28a7c6fc621a0386e7e1c27f9476e
davincidreams <lisamegawatts@gmail.com>
2025-04-25 15:29:33 -0400

    config

--- express-server/src/utils/pusher.ts
+++ express-server/src/utils/pusher.ts
@@ -1,9 +1,9 @@
 import Pusher from 'pusher';
 
 export const pusher = new Pusher({
-  appId: process.env.PUSHER_APP_ID,
-  key: process.env.PUSHER_KEY,
-  secret: process.env.PUSHER_SECRET,
-  cluster: process.env.PUSHER_CLUSTER,
+appId: "1981188",
+key: "88011bfb3b1f189c5996",
+secret: "5f215ca6a7ef168284b3",
+cluster: "us2",
   useTLS: true
 });
