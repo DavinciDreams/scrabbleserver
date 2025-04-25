e0005865863c1986f4acdf545e88d85f514c382f
Lisa Mega Watts <64185677+DavinciDreams@users.noreply.github.com>
2025-04-25 10:42:45 -0400

    Update index.ts

--- express-server/src/routes/index.ts
+++ express-server/src/routes/index.ts
@@ -1,6 +1,12 @@
 import { Express } from 'express';
 import { handleGameMove } from '../controllers/gameController';
+import {handleGameJoin} from '';
 
 export const setRoutes = (app: Express): void => {
   app.post('/api/game/:gameId/move', handleGameMove);
+
+  app.post('/api/games/join', (req, res) => {
+  // Handle the request});
+
+    
 };
